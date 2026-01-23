6c8f8a69ad24724ffb35c9d22c4d8a5d71afc26f
cuongdev1108 <cuonghk1108@gmail.com>
2026-01-23 12:26:57 +0700

    Add dark mode and light mode toggle (Tailwind class-based)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './App.tsx',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './context/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
