4483c86418684522e9ca1fcfab939913e5409c6c
cuongdev1108 <cuonghk1108@gmail.com>
2026-01-23 11:40:21 +0700

    Fix closing tag for BrowserRouter

--- App.tsx
+++ App.tsx
@@ -101,7 +101,7 @@ const App: React.FC = () => {
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </DataProvider>
   );
 };
